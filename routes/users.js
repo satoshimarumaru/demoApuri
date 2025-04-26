46eb94fe264651b7bb2b83677d4e3dce931b6173
satoshimarumaru <2003429satoshi@gmail.com>
2025-04-26 13:02:38 +0900

    ログインログアウトの改善

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,16 @@ router.get("/register", (req,res) => {
     res.render("users/register")
 })
 
-router.post("/register", async(req,res ) => {
+router.post("/register", async(req,res,next ) => {
     try{
         const {email, username, password} = req.body
     const user = new User({email, username})
     const registeredUser = await User.register(user, password)      
-    console.log(registeredUser)
-    req.flash("success", "ようこそYelp Campへ!")
-    res.redirect("/campgrounds")
+    req.login(registeredUser, err => {
+        if(err) return next(err)
+            req.flash("success", "ようこそYelp Campへ!")
+        res.redirect("/campgrounds")        
+    })
     } catch(e){
         req.flash("error", e.message)
         res.redirect("/register")
@@ -31,8 +33,14 @@ router.post("/login", passport.authenticate("local", {failureFlash:true, failure
     res.redirect("/campgrounds")
 })
 
-
-
-
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err); 
+        }
+        req.flash("success", "ログアウトしました");
+        res.redirect("/campgrounds");
+    });
+});
 
 module.exports = router
